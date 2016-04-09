2ca84ba92f519dc4176927e1d2a380979c85a040
Clark Zhang <mail@clarkok.com>
2016-04-09 14:25:46 +0800

    update api recv_for

--- software/kernel/proc/message.c
+++ software/kernel/proc/message.c
@@ -3,6 +3,18 @@
 #include "message.h"
 #include "proc.h"
 
+static inline void
+_proc_msg_dispatch(Process *dst_proc, Message *msg)
+{
+    list_append(&dst_proc->messages, &msg->_link);
+    if (
+        (dst_proc->state == PS_WAITING) &&
+        (!dst_proc->waiting_for || dst_proc->waiting_for == current_process->id)
+    ) {
+        proc_unblock(dst_proc->id, msg->length);
+    }
+}
+
 int
 proc_msg_do_send(size_t dst, size_t length, const void *content)
 {
@@ -16,15 +28,7 @@ proc_msg_do_send(size_t dst, size_t length, const void *content)
     new_msg->dst = dst;
     new_msg->length = length;
     memcpy(new_msg->content, content, length);
-
-    list_append(&dst_proc->messages, &new_msg->_link);
-
-    if (
-        (dst_proc->state == PS_WAITING) &&
-        (!dst_proc->waiting_for || dst_proc->waiting_for == current_process->id)
-    ) {
-        proc_unblock(dst_proc->id, length);
-    }
+    _proc_msg_dispatch(dst_proc, new_msg);
 
     return 1;
 }
@@ -50,12 +54,13 @@ proc_msg_do_wait_for(size_t src)
 }
 
 int
-proc_msg_do_recv_for(size_t src, char *buffer)
+proc_msg_do_recv_for(size_t src, size_t *actual_src, char *buffer)
 {
     if (src) {
         list_for_each(&current_process->messages, node) {
             Message *msg = list_get(node, Message, _link);
             if (msg->src == src) {
+                *actual_src = src;
                 memcpy(buffer, msg->content, msg->length);
                 list_unlink(node);
                 free(msg);
@@ -67,6 +72,7 @@ proc_msg_do_recv_for(size_t src, char *buffer)
         if (list_size(&current_process->messages)) {
             LinkedNode *node = list_head(&current_process->messages);
             Message *msg = list_get(node, Message, _link);
+            *actual_src = msg->src;
             memcpy(buffer, msg->content, msg->length);
             list_unlink(node);
             free(msg);
@@ -79,3 +85,17 @@ proc_msg_do_recv_for(size_t src, char *buffer)
 size_t
 proc_msg_do_get_msg_nr()
 { return list_size(&current_process->messages); }
+
+void
+proc_msg_signal(size_t dst, size_t length, const void *content)
+{
+    Process *dst_proc = proc_get_by_id(dst);
+    assert(dst_proc);
+
+    Message *new_sig = malloc(sizeof(Message) + length);
+    new_sig->src = SIGNAL_SRC;
+    new_sig->dst = dst;
+    new_sig->length = length;
+    memcpy(new_sig->content, content, length);
+    _proc_msg_dispatch(dst_proc, new_sig);
+}
--- software/kernel/proc/message.h
+++ software/kernel/proc/message.h
@@ -3,6 +3,21 @@
 
 #include "utils/linked-list.h"
 
+typedef enum SignalType
+{
+    ST_UNKNOWN = 0,
+    ST_TERM,            // request this process to quit
+    ST_CHILD_TERM,      // a child from this process terminated
+
+    SIGNAL_TYPE_NR
+} SignalType;
+
+typedef struct SignalChildTerm
+{
+    SignalType type;
+    size_t child_pid;
+} SignalChildTerm;
+
 typedef struct Message
 {
     LinkedNode _link;
@@ -15,7 +30,11 @@ typedef struct Message
 
 int proc_msg_do_send(size_t dst, size_t length, const void *content);
 size_t proc_msg_do_wait_for(size_t src);
-int proc_msg_do_recv_for(size_t src, char *buffer);
+int proc_msg_do_recv_for(size_t src, size_t *actual_src, char *buffer);
 size_t proc_msg_do_get_msg_nr();
+
+#define SIGNAL_SRC  (0xFFFFFFFFu)
+
+void proc_msg_signal(size_t dst, size_t length, const void *content);
 
 #endif
